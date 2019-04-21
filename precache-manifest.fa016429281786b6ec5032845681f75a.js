4902159f4ea999064a341b6dca65dc69078292b5
David Mihal <david@dmihal.com>
2019-04-21 10:29:32 +0200

    Updates

---
+++ precache-manifest.fa016429281786b6ec5032845681f75a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4007947f25b8724c7401",
+    "url": "/bounty-hunter/static/js/runtime~main.308013d6.js"
+  },
+  {
+    "revision": "cda12fc68487d7c7c524",
+    "url": "/bounty-hunter/static/js/main.bba965bf.chunk.js"
+  },
+  {
+    "revision": "fc76a177bcd652b35c4e",
+    "url": "/bounty-hunter/static/js/2.081a36d4.chunk.js"
+  },
+  {
+    "revision": "cda12fc68487d7c7c524",
+    "url": "/bounty-hunter/static/css/main.018cbba2.chunk.css"
+  },
+  {
+    "revision": "6a519f2832a8181bbdf6626c39fce1c6",
+    "url": "/bounty-hunter/index.html"
+  }
+];
